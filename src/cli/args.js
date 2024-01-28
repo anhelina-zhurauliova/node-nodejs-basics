a894fdf373e6cb45ad314a6d229ab8e18d5bd4e1
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2024-01-28 21:40:05 +0100

    feat: add solution for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
-const parseArgs = () => {
-    // Write your code here 
+export const parseArgs = () => {
+  const args = process.argv.slice(2);
+
+  args.forEach((arg, index) => {
+    arg.includes("--") &&
+      process.stdout.write(
+        `${arg.replace("--", "")} is ${args[index + 1]}${
+          index !== args.length - 2 ? ", " : ""
+        }`
+      );
+  });
+
+  process.stdout.write("\n");
 };
 
 parseArgs();
