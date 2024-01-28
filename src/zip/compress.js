b6282e1ee813df86a2b18d96e299d158b12bbeb4
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2024-01-28 22:11:47 +0100

    feat: add solution for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "path";
+import { createGzip } from "zlib";
+
+import { getCurrentDirectory } from "../utils/getCurrentDir.js";
+
+const __dirname = getCurrentDirectory(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+  const filesFolder = join(__dirname, "files");
+  const file = join(filesFolder, "fileToCompress.txt");
+  const destinationFile = join(filesFolder, "archive.gz");
+
+  pipeline(
+    createReadStream(file),
+    createGzip(),
+    createWriteStream(destinationFile),
+    (err) => {
+      err && console.error(err.message);
+    }
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "path";
+import { createUnzip } from "zlib";
+
+import { getCurrentDirectory } from "../utils/getCurrentDir.js";
+
+const __dirname = getCurrentDirectory(import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+  const filesFolder = join(__dirname, "files");
+  const file = join(filesFolder, "archive.gz");
+  const destinationFile = join(filesFolder, "fileToCompress.txt");
+
+  pipeline(
+    createReadStream(file),
+    createUnzip(),
+    createWriteStream(destinationFile),
+    (err) => {
+      err && console.error(err.message);
+    }
+  );
 };
 
 await decompress();
